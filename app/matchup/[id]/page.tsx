3dd722c0b3d696b094ae531b27def88a4a5ac0ba
jtaffetisaza <jacobtaffetisaza@gmail.com>
2026-09-09 09:51:21 -0400

    matchup 2

--- app/matchup/[id]/page.tsx
+++ app/matchup/[id]/page.tsx
@@ -2,6 +2,8 @@ import { notFound } from 'next/navigation';
 import Link from 'next/link';
 import { supabase } from '@/lib/supabase';
 
+export const dynamic = 'force-dynamic';
+
 interface Matchup {
   id: number;
   season: number;
